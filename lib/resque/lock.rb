7b716da13126311ce533b83b2fb537af3df6b557
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2011-05-16 22:03:39 +0300

    Added lock on create and release on finish/error

--- lib/resque/lock.rb
+++ lib/resque/lock.rb
@@ -1,27 +1,35 @@
 module Resque
   module Lock
     
-    def lock_key( options = {} )
-      "lock:#{self.class.name}-#{options.keys.map(&:to_s).sort.join("|")}-#{options.values.map(&:to_s).sort.join("|")}"
+    def lock_key( klass, options = {} )
+      "lock:#{klass}-#{options.keys.map(&:to_s).sort.join("|")}-#{options.values.map(&:to_s).sort.join("|")}"
     end
     
     def locked?( options )
       Resque.redis.exists( lock_key( options ) )
     end
     
-    def lock_uuid( options )
-      Resque.redis.get( lock_key( options ) )
+    def lock_uuid( klass, options )
+      Resque.redis.get( lock_key( klass, options ) )
     end
     
     # Where the magic happens.
-    def self.enqueue(klass, options = {})
+    def enqueue(klass, options = {})
       # Abort if another job added.
-      uuid = lock_uuid( options )
+      uuid = lock_uuid( klass, options )
       if uuid.blank?
-        uuid = super(klass, options = {})
-        Resque.redis.set( lock_key( options ), uuid )
+        uuid = super(klass, options)
+        Resque.redis.set( lock_key( klass, options ), uuid )
       end
       uuid
     end  
+    
+    def around_perform_lock *args
+      begin
+        yield
+      ensure
+        Resque.redis.del( lock_key( self.name, args[1] ) )       
+      end
+    end
   end
 end
