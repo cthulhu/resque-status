1ab843b483cce7fef65b6981cdcfe30d66c226d4
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2011-05-16 19:05:22 +0300

    Added Lock

--- lib/resque/lock.rb
+++ lib/resque/lock.rb
@@ -1,10 +1,26 @@
 module Resque
   module Lock
+    
     def lock(*args)
       "lock:#{name}-#{args.to_s}"
     end
+    
     def locked?(*args)
       Resque.redis.exists(lock(*args))
     end
+    
+    # Where the magic happens.
+    def around_perform_lock(*args)
+      # Abort if another job has created a lock.
+      return unless Resque.redis.setnx(lock(*args), true)
+
+      begin
+        yield
+      ensure
+        # Always clear the lock when we're done, even if there is an
+        # error.
+        Resque.redis.del(lock(*args))
+      end
+    end  
   end
 end
