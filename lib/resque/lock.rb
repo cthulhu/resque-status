5e2131f53883efc9a77f7edb662c51bd5f3a6dac
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2011-05-16 22:48:44 +0300

    Refactored

--- lib/resque/lock.rb
+++ lib/resque/lock.rb
@@ -1,25 +1,25 @@
 module Resque
   module Lock
     
-    def lock_key( klass, options = {} )
-      "lock:#{klass}-#{options.keys.map(&:to_s).sort.join("|")}-#{options.values.map(&:to_s).sort.join("|")}"
+    def lock_key( options = {} )
+      "lock:#{self.name}-#{options.keys.map(&:to_s).sort.join("|")}-#{options.values.map(&:to_s).sort.join("|")}"
     end
     
     def locked?( options )
       Resque.redis.exists( lock_key( options ) )
     end
     
-    def lock_uuid( klass, options )
-      Resque.redis.get( lock_key( klass, options ) )
+    def lock_uuid( options )
+      Resque.redis.get( lock_key( options ) )
     end
     
     # Where the magic happens.
     def enqueue(klass, options = {})
       # Abort if another job added.
-      uuid = lock_uuid( klass, options )
+      uuid = lock_uuid( options )
       if uuid.blank?
         uuid = super(klass, options)
-        Resque.redis.set( lock_key( klass, options ), uuid )
+        Resque.redis.set( lock_key( options ), uuid )
       end
       uuid
     end  
@@ -28,7 +28,7 @@ module Resque
       begin
         yield
       ensure
-        Resque.redis.del( lock_key( self.name, args[1] ) )       
+        Resque.redis.del( lock_key( args[1] ) )       
       end
     end
   end
