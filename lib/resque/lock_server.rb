2e16bf661e76cef01a95425fa0bd515e8d0e4121
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2011-08-25 13:20:37 +0300

    Added templates for locks

--- lib/resque/lock_server.rb
+++ lib/resque/lock_server.rb
@@ -7,6 +7,12 @@ module Resque
     VIEW_PATH = File.join(File.dirname(__FILE__), 'lock_server', 'views')
     
     def self.registered(app)
+    
+      app.get '/locks' do
+        @locks = []
+        lock_view(:locks)
+      end
+
     end
 
     app.tabs << "Locks"
