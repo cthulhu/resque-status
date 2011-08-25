7cec5c53fc421cd42bd6c332c189e071837b76ad
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2011-08-25 14:15:58 +0300

    Added templates for locks

--- lib/resque/lock_server.rb
+++ lib/resque/lock_server.rb
@@ -1,5 +1,6 @@
-require 'resque/status'
-require 'resque/lock'
+#require 'resque/status'
+#require 'resque/lock'
+require "cgi"
 
 module Resque
   module LockServer
@@ -9,13 +10,32 @@ module Resque
     def self.registered(app)
     
       app.get '/locks' do
-        @locks = []
+        @start = params[:start].to_i
+        @end = @start + ( params[:per_page] || 50 )
+        @locks = Resque.redis.keys("lock:*") + Resque.redis.keys("competitive-lock:*")
         lock_view(:locks)
       end
-
+      
+      app.helpers do
+        def lock_view(filename, options = {}, locals = {})
+          erb(File.read(File.join(::Resque::LockServer::VIEW_PATH, "#{filename}.erb")), options, locals)
+        end
+      end
+      
+      app.post '/locks/:id/kill' do
+        Resque.redis.del( params[:id] )
+        redirect u(:statuses)
+      end
+      
+      app.post '/locks/clear' do
+        Resque.redis.keys("lock:*").each{|k| Resque.redis.del( k ) }
+        redirect u(:locks)
+      end
+      
+      
+      app.tabs << "Locks"
     end
 
-    app.tabs << "Locks"
   end
 end
 Resque::Server.register Resque::LockServer
